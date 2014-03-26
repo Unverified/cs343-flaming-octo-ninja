6672d9e8ef8e72fd1cd9b74d6c15c35b2a3a5de1
Nick F <nich.fagan@gmail.com>
2014-03-25 20:28:09 -0400

    Completed printer, based of my a5 printer. Simple monitor, not a coroutine monitor.

--- printer.cc
+++ printer.cc
@@ -1,28 +1,142 @@
+#include <iostream>
+#include <list>
 #include "printer.h"
+
+using namespace std;
 
 Printer::Printer( unsigned int numStudents, unsigned int numVendingMachines, unsigned int numCouriers ) 
   : nStudents( numStudents ), nVendingMachines( numVendingMachines ), nCouriers( numCouriers ) {
+
+  mNumCols = 5 + nStudents + nVendingMachines + nCouriers;
+  mCurCol = 0;
+  printStart();
+}
+
+Printer::~Printer() {
+  printEnd();
+}
+
+void Printer::printStart() {
+  cout << "Parent\tWATOff\tNames\tTruck\tPlant";
+
+  for(unsigned int i = 0; i < nStudents; i++) {
+    cout << "\tStud" << i;
+  }
+
+  for(unsigned int i = 0; i < nVendingMachines; i++) {
+    cout << "\tMach" << i;
+  }
+
+  for(unsigned int i = 0; i < nCouriers; i++) {
+    cout << "\tCour" << i;
+  }
+  cout << endl;
+
+  for(unsigned int i = 0; i < mNumCols; i++) {
+    if(i > 0) cout << "\t";
+    cout << "*******";
+  }
+  cout << endl;
+}
+
+void Printer::printEnd() {
+  flush();
+  cout << "\n**********************" << endl;
+}
+
+void Printer::flush() {
+  for( ; !mBuffer.empty(); mCurCol++) {	// Constintly loop, printing whats at the front of the buffer and breaking if the buffer is empty
+    Entry entry = mBuffer.front();
+    if(entry.col < mCurCol) {		// If the next id in the buffer is less than the current column we are printing we need to wrap to the next line
+      cout << endl;			// Print a newline
+      mCurCol = 0;				// Set the column we are printing to the first one, ie 0
+    }
+ 
+    if(mCurCol != 0) cout << "\t";		// Print a tab character is the column we are printing is no the first one
+    if(entry.col == mCurCol) {		// If the column we are printing matches the next id in the buffer then print that entry
+      entry.print();			// Prints the entry
+      mBuffer.pop_front();		// Remove the entry from the buffer
+    }
+  }
+}
+
+Printer::Entry Printer::createBufferEntry(unsigned int col, char state, int value1, int value2, bool finisher) {
+  Entry entry;
+  entry.col = col;
+  entry.state = state;
+  entry.value1 = value1;
+  entry.value2 = value2;
+  entry.finisher = finisher;
+  return entry;
+}
+
+void Printer::addBufferEntry(unsigned int col, char state) {
+  addBufferEntry(col, state, -1, -1, false);
+}
+
+void Printer::addBufferEntry(unsigned int col, char state, int value1) {	
+  addBufferEntry(col, state, value1, -1, false);
+}
+
+void Printer::addBufferEntry(unsigned int col, char state, int value1, int value2) {	
+  addBufferEntry(col, state, value1, value2, false);
+}
+
+void Printer::addBufferEntry(unsigned int col, char state, int value1, int value2, bool finisher) {	// Add an entry to the back of the buffer
+  list<Entry>::iterator it;
+
+  for (it = mBuffer.begin();;it++) {
+    if(it == mBuffer.end() || it->col > col) {
+      break;
+    } else if(it->col == col) {
+      flush();
+      it = mBuffer.begin();
+      break;
+    }
+  }
+  
+  mBuffer.insert(it, createBufferEntry(col, state, value1, value2, finisher));
+}
+
+void Printer::fillBufferWithEllipses(unsigned int finishedCol) {		// Fill all the empty spaces in the buffer with finished state
+  for(unsigned int i = 0; i < mNumCols; i++) {		// Add entries behind of the finished task in the buffer
+    if(i != finishedCol) addBufferEntry(i, 'F');
+  }
+}
+
+int Printer::getCol(Kind kind, unsigned int lid) {
+  switch(kind) {
+    case Student:
+      return 5 + lid;
+    case Vending:
+      return 5 + nStudents + lid;
+    case Courier:
+      return 5 + nStudents + nVendingMachines + lid;
+    default:
+      return kind;
+  }
 }
 
 void Printer::print( Kind kind, char state ) {
+  addBufferEntry(getCol(kind, 0), state);
 }
 
 void Printer::print( Kind kind, char state, int value1 ) {
+  addBufferEntry(getCol(kind, 0), state, value1);
 }
 
 void Printer::print( Kind kind, char state, int value1, int value2 ) {
+  addBufferEntry(getCol(kind, 0), state, value1, value2);
 }
 
 void Printer::print( Kind kind, unsigned int lid, char state ) {
+  addBufferEntry(getCol(kind, lid), state);
 }
 
 void Printer::print( Kind kind, unsigned int lid, char state, int value1 ) {
+  addBufferEntry(getCol(kind, lid), state, value1);
 }
 
 void Printer::print( Kind kind, unsigned int lid, char state, int value1, int value2 ) {
+  addBufferEntry(getCol(kind, lid), state, value1, value2);
 }
-
-void Printer::main() {
-
-}
-
--- printer.h
+++ printer.h
@@ -1,21 +1,59 @@
 #ifndef __PRINTER_H__
 #define __PRINTER_H__
 
-_Cormonitor Printer {
+#include <list>
+
+_Monitor Printer {
+    struct Entry {					// Holds the info that should be print, is store in the buffer
+      unsigned int col;						// The column this Entry should be printed in
+      char state;					
+      int value1;
+      int value2;
+      bool finisher;					// Indicates if this is the entry that caused a finish
+
+      void print() {					// Print this buffer entry
+        if(state == 'F' && !finisher) {	// If the entry is a Finished state and this is not the entry that caused the finish then print "..."
+          std::cout << "...";
+        } else {					// Else print the contents of this entry
+          std::cout << state ;				// Print the state
+          if(value1 > -1) std::cout << value1;		// Print value1 if there is one
+          if(value2 > -1) std::cout << "," << value2;	// Print value2 if there is one
+        }
+      }
+    };
+
     const unsigned int nStudents;
     const unsigned int nVendingMachines;
     const unsigned int nCouriers;
 
-    void main();    
+    unsigned int mNumCols;
+    unsigned int mCurCol;
+
+    std::list<Entry> mBuffer;
+
+    void printStart();
+    void printEnd();
+
+    Entry createBufferEntry(unsigned int col, char state, int value1, int value2, bool finisher);	// Create a basic buffer entry with just id and state
+    void addBufferEntry(unsigned int col, char state);				// Add a buffer entry, flushes if the entry already exists
+    void addBufferEntry(unsigned int col, char state, int value1);		// Add a buffer entry, flushes if the entry already exists
+    void addBufferEntry(unsigned int col, char state, int value1, int value2);	// Add a buffer entry, flushes if the entry already exists
+    void addBufferEntry(unsigned int col, char state, int value1, int value2, bool finisher);	// Add a buffer entry, flushes if the entry already exists
+    void flush();							// Flush the buffer
+    void fillBufferWithEllipses(unsigned int finisherCol);			// Fill the buffer with Finish states that will be printed as ...
   public:
     enum Kind { Parent, WATCardOffice, NameServer, Truck, BottlingPlant, Student, Vending, Courier };
     Printer( unsigned int numStudents, unsigned int numVendingMachines, unsigned int numCouriers );
+    ~Printer();
     void print( Kind kind, char state );
     void print( Kind kind, char state, int value1 );
     void print( Kind kind, char state, int value1, int value2 );
     void print( Kind kind, unsigned int lid, char state );
     void print( Kind kind, unsigned int lid, char state, int value1 );
     void print( Kind kind, unsigned int lid, char state, int value1, int value2 );
+
+  private:
+    int getCol(Kind kind, unsigned int lid);
 };
 
 #endif
