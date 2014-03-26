02fd411b38da0ff745085fed4e1d858b27c8d21b
Nick F <nich.fagan@gmail.com>
2014-03-25 22:05:03 -0400

    Created/Delete all the objects in main.cc

--- main.cc
+++ main.cc
@@ -4,6 +4,13 @@
 
 #include "config.h"
 #include "printer.h"
+#include "bank.h"
+#include "parent.h"
+#include "watcardOffice.h"
+#include "nameServer.h"
+#include "vendingMachine.h"
+#include "bottlingPlant.h"
+#include "student.h"
 
 #include "MPRNG.h"
 
@@ -47,7 +54,42 @@ void uMain::main() {
         usage( argv );
     }
 
-    Printer printer( cfgParms.numStudents, cfgParms.numVendingMachines, cfgParms.numCouriers );
+    /* Create everything, must be done in this order as per assignment spec. */
 
+    Printer printer( cfgParms.numStudents, cfgParms.numVendingMachines, cfgParms.numCouriers );
+    Bank bank( cfgParms.numStudents );
+    Parent parent( printer, bank, cfgParms.numStudents, cfgParms.parentalDelay );
+    WATCardOffice cardOffice( printer, bank, cfgParms.numCouriers );
+    NameServer nameServer( printer, cfgParms.numVendingMachines, cfgParms.numStudents );
+
+    VendingMachine *vendingMachines[cfgParms.numVendingMachines];
+    for(unsigned int i = 0; i < cfgParms.numVendingMachines; i++) {
+      vendingMachines[i] = new VendingMachine( printer, nameServer, i, cfgParms.sodaCost, cfgParms.maxStockPerFlavour );
+    }
+
+    BottlingPlant *bottlingPlant = new BottlingPlant( printer, nameServer, cfgParms.numVendingMachines, cfgParms.maxShippedPerFlavour, cfgParms.maxStockPerFlavour, cfgParms.timeBetweenShipments );
+
+    Student *students[cfgParms.numStudents];
+    for(unsigned int i = 0; i < cfgParms.numStudents; i++) {
+      students[i] = new Student( printer, nameServer, cardOffice, i, cfgParms.maxPurchases );
+    }
+    
+    /* Delete all the tasks */
+
+    for(unsigned int i = 0; i < cfgParms.numStudents; i++) {
+      delete students[i];
+    }
+
+    delete bottlingPlant;	// Must be done before deleting vending machines or deadlock can occur
+
+    for(unsigned int i = 0; i < cfgParms.numVendingMachines; i++) {
+      delete vendingMachines[i];
+    }
 }
+
+
+
+
+
+
 
